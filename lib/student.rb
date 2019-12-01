c6b4c8eefb3bf9a1cf44ab73fc0d16cf1a83c87a
generous-vector-9581 <ojappah@gmail.com>
2019-12-01 20:30:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,11 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  
+    def self.new_from_db(row)
+    # create a new Student object given a row from the database	    
+    student = self.new
+    student.id, student.name, student.grade = row[0], row[1], row[2]
+    student
+  end
 
 end
