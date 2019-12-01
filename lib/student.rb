d3e6e8f4719b315369243498fd9c424891c7ebf1
generous-vector-9581 <ojappah@gmail.com>
2019-12-01 19:56:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,6 +45,11 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
+  
+    def self.first_student_in_grade_10
+    student = self.first_x_students_in_grade_10(1).flatten
+    self.new_from_db(student)
+  end
     def self.all_students_in_grade_X(num)
     sql = "SELECT * FROM students WHERE grade=?;"
     DB[:conn].execute(sql, num)
